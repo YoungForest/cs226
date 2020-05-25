9930a8dbe3bd15069eaf646a42a751dcd99f358b
杨森 <sen.yang96@outlook.com>
2020-05-25 17:30:47 +0800

    week3 success

--- week3/BruteCollinearPoints.java
+++ week3/BruteCollinearPoints.java
@@ -11,18 +11,6 @@ import edu.princeton.cs.algs4.In;
 public class BruteCollinearPoints {
     private LineSegment[] lineSegments;
 
-    private boolean contain(Point x1, Point y1, Point x2, Point y2) {
-        if (x1.slopeTo(y1) == x2.slopeTo(y2)) {
-            if (x1.compareTo(x2) == 0 || x1.slopeTo(x2) == x1.slopeTo(y1)) {
-                return x1.compareTo(x2) <= 0 && y1.compareTo(y2) >= 0;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
-
     public BruteCollinearPoints(Point[] points) { // finds all line segments containing 4 points
         if (points == null) {
             throw new IllegalArgumentException("Argument is null");
@@ -33,7 +21,7 @@ public class BruteCollinearPoints {
                 throw new IllegalArgumentException("The " + i + " st point is null");
             }
         }
-        List<Point[]> lines = new ArrayList<>();
+        List<LineSegment> lines = new ArrayList<>();
         Arrays.sort(points);
         for (int i = 0; i < n - 1; ++i) {
             if (points[i].compareTo(points[i + 1]) == 0) {
@@ -46,46 +34,15 @@ public class BruteCollinearPoints {
                     for (int l = k + 1; l < n; ++l) {
                         if (points[i].slopeTo(points[j]) == points[j].slopeTo(points[k])
                                 && points[j].slopeTo(points[k]) == points[k].slopeTo(points[l])) {
-                            Point[] line = new Point[2];
-                            line[0] = points[i];
-                            line[1] = points[l];
-                            lines.add(line);
+                            lines.add(new LineSegment(points[i], points[l]));
+                            System.out.println(i);
+                            System.out.println(l);
                         }
                     }
                 }
             }
         }
-        ArrayList<LineSegment> ans = new ArrayList<>();
-        lines.sort((a, b) -> {
-            if (a[0].compareTo(b[0]) != 0) return a[0].compareTo(b[0]);
-            else return a[1].compareTo(b[1]);
-        });
-        if (!lines.isEmpty()) {
-            ArrayList<Point[]> unique = new ArrayList<>();
-            unique.add(lines.get(0));
-            for (int i = 1; i < lines.size(); ++i) {
-                if (lines.get(i)[0] == lines.get(i-1)[0] && lines.get(i)[1] == lines.get(i-1)[1]) {
-                    continue;
-                } else {
-                    unique.add(lines.get(i));
-                }
-            }
-            lines = unique;
-        }
-        for (int i = 0; i < lines.size(); ++i) {
-            boolean wasContain = false;
-            for (int j = 0; j < lines.size(); ++j) {
-                if (i == j) continue;
-                if (contain(lines.get(j)[0], lines.get(j)[1], lines.get(i)[0], lines.get(i)[1])) {
-                    wasContain = true;
-                    break;
-                }
-            }
-            if (!wasContain) {
-                ans.add(new LineSegment(lines.get(i)[0], lines.get(i)[1]));
-            }
-        }
-        lineSegments = ans.toArray(new LineSegment[0]);
+        lineSegments = lines.toArray(new LineSegment[lines.size()]);
     }
 
     public int numberOfSegments() {
--- week3/FastCollinearPoints.java
+++ week3/FastCollinearPoints.java
@@ -13,18 +13,6 @@ import edu.princeton.cs.algs4.In;
 public class FastCollinearPoints {
     private LineSegment[] lineSegments;
 
-    private boolean contain(Point x1, Point y1, Point x2, Point y2) {
-        if (x1.slopeTo(y1) == x2.slopeTo(y2)) {
-            if (x1.compareTo(x2) == 0 || x1.slopeTo(x2) == x1.slopeTo(y1)) {
-                return x1.compareTo(x2) <= 0 && y1.compareTo(y2) >= 0;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
-
     public FastCollinearPoints(Point[] points) {
         if (points == null) {
             throw new IllegalArgumentException("Argument is null");
@@ -35,7 +23,7 @@ public class FastCollinearPoints {
                 throw new IllegalArgumentException("The " + i + " st point is null");
             }
         }
-        ArrayList<Point[]> lines = new ArrayList<>();
+        ArrayList<LineSegment> lines = new ArrayList<>();
         Arrays.sort(points);
         for (int i = 0; i < n - 1; ++i) {
             if (points[i].compareTo(points[i + 1]) == 0) {
@@ -65,54 +53,18 @@ public class FastCollinearPoints {
                     if (slope == p.slopeTo(end)) {
                     } else {
                         if (r - l >= 3) {
-                            Point[] line = new Point[2];
-                            line[0] = p;
-                            line[1] = others.get(r - 1);
-                            lines.add(line);
+                            lines.add(new LineSegment(p, others.get(r - 1)));
                         }
                         l = r;
                         slope = p.slopeTo(end);
                     }
                 }
                 if (others.size() - l >= 3) {
-                    Point[] line = new Point[2];
-                    line[0] = p;
-                    line[1] = others.get(others.size() - 1);
-                    lines.add(line);
+                    lines.add(new LineSegment(p, others.get(others.size() - 1)));
                 }
             }
         }
-        ArrayList<LineSegment> ans = new ArrayList<>();
-        lines.sort((a, b) -> {
-            if (a[0].compareTo(b[0]) != 0) return a[0].compareTo(b[0]);
-            else return a[1].compareTo(b[1]);
-        });
-        if (!lines.isEmpty()) {
-            ArrayList<Point[]> unique = new ArrayList<>();
-            unique.add(lines.get(0));
-            for (int i = 1; i < lines.size(); ++i) {
-                if (lines.get(i)[0] == lines.get(i-1)[0] && lines.get(i)[1] == lines.get(i-1)[1]) {
-                    continue;
-                } else {
-                    unique.add(lines.get(i));
-                }
-            }
-            lines = unique;
-        }
-        for (int i = 0; i < lines.size(); ++i) {
-            boolean wasContain = false;
-            for (int j = 0; j < lines.size(); ++j) {
-                if (i == j) continue;
-                if (contain(lines.get(j)[0], lines.get(j)[1], lines.get(i)[0], lines.get(i)[1])) {
-                    wasContain = true;
-                    break;
-                }
-            }
-            if (!wasContain) {
-                ans.add(new LineSegment(lines.get(i)[0], lines.get(i)[1]));
-            }
-        }
-        lineSegments = ans.toArray(new LineSegment[0]);
+        lineSegments = lines.toArray(new LineSegment[0]);
     } // finds all line segments containing 4 or more points
 
     public int numberOfSegments() {
